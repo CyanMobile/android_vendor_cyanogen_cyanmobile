dd7763076f0f88a0d69013181cd957f8b445d1da
fuzz <fuzzzzzzzzzzzzzzzzzz@gmail.com>
2013-01-10 08:58:01 +0900

    jordan: Don't override BUILD_VERSION

--- products/cyanogen_jordan.mk
+++ products/cyanogen_jordan.mk
@@ -37,7 +37,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE=umts_jordan \
     BUILD_FINGERPRINT=MOTO/MB526_RTDACH/umts_jordan:2.3.4/4.5.1-134_DFP-74/1313117579:user/release-keys \
     PRIVATE_BUILD_DESC="umts_jordan_emara-user 2.3.4 4.5.1-134_DFP-74 1313117579 release-keys" \
-    BUILD_NUMBER=${DATE} \
     BUILD_VERSION_TAGS=release-keys \
     TARGET_BUILD_TYPE=user
 
@@ -46,5 +45,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 #
 PRODUCT_COPY_FILES += \
     vendor/cyanogen/prebuilt/hdpi/media/bootanimation.zip:system/media/bootanimation.zip
-
-
